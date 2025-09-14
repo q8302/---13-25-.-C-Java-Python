4e93072856e983c5435ee26a41713e64c730e147
q8302 <chadka17@mail.ru>
2025-09-14 23:22:54 +0300

    Update 2025_09_14_20_14_22.cpp

--- 2025_09_14_20_14_22.cpp
+++ 2025_09_14_20_14_22.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <stack>
- 
-int main()
-{
-    std::stack<std::string> stack;
-    if(stack.empty())
-    {
-        std::cout << "stack is empty" << std::endl;
-    }
-    std::cout << "stack size: " << stack.size() << std::endl;
-}
+
